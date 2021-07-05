8f194f2eff843c441ea2fe84457ffade45ef8f2d
JangJooHyuck <unodos4@infobank.net>
2021-07-05 16:36:14 +0900

    수정함

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
     public static void main(String args[]) {
         System.out.println("hello world");
 
-        System.out.println("vscode3");
+        System.out.println("vscode3수정됬나");
     }
 }
