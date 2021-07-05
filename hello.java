bf37c33447c4ee876c910cba73f06d8692d39289
JangJooHyuck <unodos4@infobank.net>
2021-07-05 16:44:29 +0900

    vs에서수정

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String args[]) {
         System.out.println("hello world");
-        System.out.println("vscode23222");
+        System.out.println("evscode23222");
     }
 }
