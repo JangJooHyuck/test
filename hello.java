00ea9c7c7272cbc8f74250b6074f3771450188f6
JangJooHyuck <unodos4@infobank.net>
2021-07-05 17:08:27 +0900

    수정했습니다

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String args[]) {
         System.out.println("hello world");
-        System.out.println("수정222");
+        System.out.println("qeweweqwe2");
     }
 }
