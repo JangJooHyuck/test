6af2a39aa9953feb3f679e3edf492288520d1bc6
JangJooHyuck <unodos4@infobank.net>
2021-07-06 16:30:02 +0900

    qwe

--- Emailcheck.java
+++ Emailcheck.java
@@ -7,15 +7,14 @@ public class Emailcheck {
         Scanner sc = new Scanner(System.in); // 스캐너 객체생성
         System.out.println("이메일을 입력하세요."); // 사용자로부터 이메일 입력받기
         String email = sc.next(); // 입력받은 문자를 email에 저장
-        String emailRegex = "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+"; // 이메일 정규 표현식
-        if (email.matches(emailRegex)) { // email 의 값이 emaliRegex 의 조건과 맞으면
-            System.out.println("이메일이 형식이 정상적입니다.");
+        String emailpattern = "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+"; // 이메일 정규 표현식
+        if (email.matches(emailpattern)) { // email 의 값이 emaliRegex 의 조건과 맞으면
+            System.out.println("이메일의 형식이 정상적입니다.");
             System.out.println("당신의 이메일은 " + email + " 입니다.");
         } else { // 조건이 안맞으면
-            System.out.println("이메일이 형식이 정상적이지 않습니다.");
-
+            System.out.println("이메일의 형식이 정상적이지 않습니다.");
             System.out.println("입력하신 이메일은 " + email + " 입니다.");
-
         }
+
     }
 } // cmd - chcp 949 - java Emailcheck 하면됨
