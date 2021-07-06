daa127ea58392f4015f5cfdfd42c3de0df5425ee
JangJooHyuck <unodos4@infobank.net>
2021-07-06 17:08:28 +0900

    EmailChecker 최종

--- Emailcheck.java
+++ Emailcheck.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Emailcheck {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // 스캐너 객체생성
+        Scanner scan = new Scanner(System.in); // 스캐너 객체생성
         System.out.println("이메일을 입력하세요."); // 사용자로부터 이메일 입력받기
-        String email = sc.next(); // 입력받은 문자를 email에 저장
-        String emailpattern = "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+"; // 이메일 정규 표현식
+        String email = scan.next(); // 입력받은 문자를 email에 저장
+        String emailpattern = "[a-zA-Z0-9_.+-]+@[a-zA-Z]+\\.[a-zA-Z]+"; // 이메일 정규 표현식
         if (email.matches(emailpattern)) { // email 의 값이 emaliRegex 의 조건과 맞으면
             System.out.println("이메일의 형식이 정상적입니다.");
             System.out.println("당신의 이메일은 " + email + " 입니다.");
