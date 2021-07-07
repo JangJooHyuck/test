684f05649a0ae582fe82a17ff1cb8baec577e82c
JangJooHyuck <unodos4@infobank.net>
2021-07-08 08:13:37 +0900

    발표준비

--- Emailcheck.java
+++ Emailcheck.java
@@ -21,7 +21,7 @@ public class Emailcheck {
         while (Id.matches(Idpattern) == false) {
 
             System.out.println("입력하신 아이디의 형식이 정상적이지 않습니다.");
-            System.out.println("아이디는 영어와 숫자로만 생성할 수 있습니다.(최대20글자), (. , _ , + , - 4개의 특수문자는 허용)");
+            System.out.println("아이디는 영어와 숫자로만 생성할 수 있습니다.(최소 1글자 ~ 20글자), (. , _ , + , - 4개의 특수문자는 허용)");
 
             // 사용자에게 재입력 요구
             System.out.println("아이디를 다시 입력해주세요.");
@@ -54,13 +54,15 @@ public class Emailcheck {
             Pw = scan.nextLine();
         }
 
-        // 마스킹관련 //replaceAll(변환하고자하는대상, 변환할 문자)
-        // https://coding-today.tistory.com/29?category=1007587
+        // 마스킹관련
+        // replaceAll (변환하고자하는대상, 변환할 문자)
+        // 문자열 {2} 범위 이상부터는 *로 치환 (ex. 12345 -> 12***)
         String maskingpw = Pw.replaceAll("(?<=.{2}).", "*");
 
         // 이메일 관련
 
         // 이메일 패턴 정의
+
         String emailpattern = "[a-zA-Z0-9_.+-]+@[a-zA-Z]+\\.[a-zA-Z]+";
 
         // 사용자로부터 이메일 입력받기
@@ -72,7 +74,7 @@ public class Emailcheck {
         // 이메일패턴이랑 이메일이 서로 일치하지 않으면
         while (email.matches(emailpattern) == false) {
             System.out.println("입력하신 이메일의 형식이 정상적이지 않습니다.");
-            System.out.println("이메일은 ** @ ** . ** 형식으로만 생성할 수 있습니다");
+            System.out.println("이메일은 ** @ **.** 형식으로만 생성할 수 있습니다");
 
             // 사용자에게 재입력 요구
             System.out.println("이메일을 다시 입력해주세요.");
@@ -85,11 +87,12 @@ public class Emailcheck {
 
         // 스캐너 닫아주고
         scan.close();
+
         // 각각 패턴에 맞게 저장된 id, pw, email 들을 표출한다.
 
         System.out.println("당신의 아이디는 " + Id + " 입니다.");
 
-        // pw 는 아까 masking 한 pw 인 maskingpw 로 출력함
+        // pw 는 아까 masking 한 maskingpw 로 출력함
         System.out.println("당신의 패스워드는 " + maskingpw + " 입니다.");
 
         System.out.println("당신의 이메일은 " + email + " 입니다.");
